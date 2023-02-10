5b124aa564ed8b91f8db75d3f96f3cb2f1cde0f2
Andrew Wright <aj268.wright@googlemail.com>
2023-02-10 08:51:20 +0000

    Restore from my local clone of this repository to my KeeperRL mods folder
    Do not use. Make your own.

---
+++ Mods/DO_NOT_USE_Softmonsters_Restore.bat
@@ -0,0 +1,3 @@
+RMDIR "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods" /S /Q
+mkdir "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods"
+xcopy /s "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods"
