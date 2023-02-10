75f7accfec59e787b86471a435a86c0a2af970d2
Andrew Wright <aj268.wright@googlemail.com>
2023-02-10 08:41:07 +0000

    Automating backup from keeperRL mods folder to my clone of this folder on my local pc
    Don't use this, write your own.

---
+++ Mods/Alpha36/DO_NOT_USE_Softmonsters_Backup.bat
@@ -0,0 +1,3 @@
+RMDIR "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36" /S /Q
+mdir "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36"
+xcopy /s "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods" "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36"
