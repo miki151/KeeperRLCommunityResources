f91b34cbae321343a87c2ea157f9ba9db415e5fc
Andrew Wright <aj268.wright@googlemail.com>
2023-03-24 13:57:58 +0000

    Technology for writing help pages

---
+++ Apps/FormatTextToHelpText/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Text As String = TextBox1.Text
+        Text = Replace(Text, vbCrLf + vbCrLf, "¬")
+        Text = Replace(Text, vbCrLf, " ")
+        Text = Replace(Text, "¬", vbCrLf + vbCrLf)
+        Dim lines As String() = Text.Split(vbCrLf)
+        Text = ""
+        For Each line As String In lines
+            Text = Text + "P(""" + line.Trim() + """)" + vbCrLf
+        Next
+        TextBox1.Text = "# View with word wrap! " + vbCrLf + vbCrLf + Text
+    End Sub
+
+End Class
