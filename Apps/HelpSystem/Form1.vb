7fcc537a174e52d6a683db98bc427e057a1b5708
Andrew Wright <aj268.wright@googlemail.com>
2023-03-24 15:26:48 +0000

    Help pages and related app changes.

--- Apps/HelpSystem/Form1.vb
+++ Apps/HelpSystem/Form1.vb
@@ -3,27 +3,13 @@
         Dim Text As String = TextBox1.Text
         Text = Replace(Text, vbCrLf + vbCrLf, "¬")
         Text = Replace(Text, vbCrLf, " ")
-        'Dim spaceSplit As String() = Split(Text, " ")
-        'Text = ""
-        'Dim pos As Integer = 0
-        'For Each s As String In spaceSplit
-        ' If pos + Len(s) > 63 Then
-        ' Text = Text + vbCrLf + s
-        ' pos = Len(s)
-        ' ElseIf Text = "" Then
-        ' Text = s
-        ' pos = Len(s)
-        ' Else
-        ' Text = Text + " " + s
-        ' pos = pos + Len(s) + 1
-        ' End If
-        'Next
         Text = Replace(Text, "¬", vbCrLf + vbCrLf)
         Dim lines As String() = Text.Split(vbCrLf)
         Text = ""
         For Each line As String In lines
             Text = Text + "P(""" + line.Trim() + """)" + vbCrLf
         Next
+        Text = Text + "P("""")" + vbCrLf
         Text = "Def P(Text) Paragraph(500, Text) End" + vbCrLf +
             "MiniWindow(Scrolling(MarginRight(30, MarginLeft(30,  " + vbCrLf +
             "Vertical {" + vbCrLf +
