4e7dd9ccb8671fffe201e49420753384d9302710
Andrew Wright <aj268.wright@googlemail.com>
2023-04-03 22:13:40 +0100

    update sprite cleaning

--- Apps/KRLDeveloper/KRLDeveloper/frmSpriteBatch.vb
+++ Apps/KRLDeveloper/KRLDeveloper/frmSpriteBatch.vb
@@ -4,39 +4,29 @@
 
         Dim count As Integer = 1
         Dim pth As String = TextBox2.Text
-        Dim outpath As String = TextBox1.Text
         For Each str As String In System.IO.Directory.GetFiles(pth)
-            Dim bmp As New Bitmap(str)
-            bmp = New Bitmap(bmp, New Size(24, 24))
-            Dim rbgs As New List(Of Color)
-            For x = 0 To bmp.Width - 1
-                For y = 0 To bmp.Height - 1
-                    If Not rbgs.Contains(bmp.GetPixel(x, y)) Then rbgs.Add(bmp.GetPixel(x, y))
-                    Application.DoEvents()
-                Next
-            Next
-            For f = 15 To 0 Step -1
-                Dim clone = bmp.Clone
-                For Each col2 As Color In rbgs
-                    Application.DoEvents()
-                    Dim av As Long = (CLng(col2.R) + CLng(col2.B) + CLng(col2.B))
-                    If av >= 256 - 4 * f Or av <= 4 * f Then
-                        clone.MakeTransparent(col2)
-                    ElseIf Rnd() > 0.75 And False Then
-                        Dim substitute As Color = Color.FromArgb(av, av, av, av)
-                        For x = 0 To bmp.Width - 1
-                            For y = 0 To bmp.Height - 1
-                                If clone.GetPixel(x, y).ToArgb = col2.ToArgb Then
-                                    clone.SetPixel(x, y, substitute)
-                                End If
-                            Next
-                        Next
-                    End If
-                Next
-                clone.Save(outpath + "\" + count.ToString() + "_" + System.Guid.NewGuid.ToString() + ".png", Imaging.ImageFormat.Png)
-                count = count + 1
-            Next f
+            WriteFile(str, Imaging.PixelFormat.Format1bppIndexed, count)
+            WriteFile(str, Imaging.PixelFormat.Format4bppIndexed, count)
+            WriteFile(str, Imaging.PixelFormat.Format8bppIndexed, count)
+            WriteFile(str, Imaging.PixelFormat.Format16bppRgb555, count)
         Next
+    End Sub
+
+    Private Sub WriteFile(str As String, format As Imaging.PixelFormat, ByRef count As Integer)
+
+        Dim bmp As New Bitmap(str)
+        bmp = bmp.Clone(New Rectangle(0, 0, bmp.Width, bmp.Height), format)
+        bmp = New Bitmap(bmp, New Size(24, 24))
+        For f = 0 To 23
+            bmp.MakeTransparent(bmp.GetPixel(0, f))
+            bmp.MakeTransparent(bmp.GetPixel(23, f))
+            bmp.MakeTransparent(bmp.GetPixel(f, 23))
+            bmp.MakeTransparent(bmp.GetPixel(f, 0))
+        Next f
+        Dim outpath As String = TextBox1.Text
+        Dim filename As String = outpath + "\" + count.ToString() + ".png"
+        bmp.Save(filename, Imaging.ImageFormat.Png)
+        count = count + 1
     End Sub
 
 End Class
