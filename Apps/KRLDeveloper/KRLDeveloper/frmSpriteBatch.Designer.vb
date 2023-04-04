a29ee124cf5f6720ecd0af3b4b50856b6cda585d
Andrew Wright <aj268.wright@googlemail.com>
2023-04-04 11:47:19 +0100

    tidying

--- Apps/KRLDeveloper/KRLDeveloper/frmSpriteBatch.Designer.vb
+++ Apps/KRLDeveloper/KRLDeveloper/frmSpriteBatch.Designer.vb
@@ -25,43 +25,76 @@ Partial Class frmSpriteBatch
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox2.Location = New System.Drawing.Point(92, 12)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(381, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(354, 20)
         Me.TextBox2.TabIndex = 5
         Me.TextBox2.Text = "C:\MessySprites"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 38)
+        Me.Button1.Location = New System.Drawing.Point(92, 47)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(174, 23)
         Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Clean up"
+        Me.Button1.Text = "Make sprites from images"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 67)
+        Me.TextBox1.Location = New System.Drawing.Point(100, 90)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(381, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(346, 20)
         Me.TextBox1.TabIndex = 7
         Me.TextBox1.Text = "C:\CleanSprites"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Input directory"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Outout directory"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(272, 47)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(174, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Make sprites from sheet"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'frmSpriteBatch
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(469, 129)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox2)
         Me.Name = "frmSpriteBatch"
-        Me.Text = "frmSpriteBatch"
+        Me.Text = "Sprite batch jobs"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -70,4 +103,7 @@ Partial Class frmSpriteBatch
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
 End Class
