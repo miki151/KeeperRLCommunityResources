19148b54bfe14086290b709d1170222309e7fe9c
Andrew Wright <aj268.wright@googlemail.com>
2023-06-16 20:54:51 +0100

    Change mod versions to A37 for some mods.

--- Mods/DO_NOT_USE_Softmonsters_Restore.bat
+++ Mods/DO_NOT_USE_Softmonsters_Restore.bat
@@ -2,4 +2,4 @@ del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\*.*" /q
 del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\*\*.*" /q
 del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\*\*\*.*" /q
 del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\*\*\*\*.*" /q
-xcopy /s /y "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods"
+xcopy /s /y "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha37" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods"
