cd2cce271af81754743ded8f71862f25b4971bd2
Andrew Wright <aj268.wright@googlemail.com>
2023-07-21 09:08:09 +0100

    Add A36 versions of batches back

---
+++ Mods/DO_NOT_USE_Softmonsters_Restore_A36.bat
@@ -0,0 +1,5 @@
+del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\*.*" /q
+del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\*\*.*" /q
+del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\*\*\*.*" /q
+del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\*\*\*\*.*" /q
+xcopy /s /y "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods"
