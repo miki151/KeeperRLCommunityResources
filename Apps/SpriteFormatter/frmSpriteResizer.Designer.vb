6b2ea4351e55f01c62388cf4501808b13f6a493f
Andrew Wright <aj268.wright@googlemail.com>
2023-09-09 14:10:36 +0100

    Sprite resizer and bonus mod biomes

---
+++ Apps/SpriteFormatter/frmSpriteResizer.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmSpriteResizer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(92, 12)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(328, 20)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.Text = "C:\MessySprites"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(92, 73)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(174, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Make sprites from images"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(100, 102)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(320, 20)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "C:\CleanSprites"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Input directory"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Outout directory"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(100, 50)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(43, 17)
+        Me.CheckBox1.TabIndex = 10
+        Me.CheckBox1.Text = "8x8"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(149, 50)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(55, 17)
+        Me.CheckBox2.TabIndex = 11
+        Me.CheckBox2.Text = "16x16"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(210, 50)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(55, 17)
+        Me.CheckBox3.TabIndex = 12
+        Me.CheckBox3.Text = "24x24"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(271, 50)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(55, 17)
+        Me.CheckBox4.TabIndex = 13
+        Me.CheckBox4.Text = "30x30"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(427, 8)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(25, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(427, 102)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(25, 23)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "..."
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'frmSpriteResizer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(464, 139)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Name = "frmSpriteResizer"
+        Me.Text = "Sprite batch jobs"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Apps/SpriteFormatter/frmSpriteResizer.vb
@@ -0,0 +1,65 @@
+﻿Public Class frmSpriteResizer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim count As Integer = 1
+        Dim pth As String = TextBox2.Text
+        For Each str As String In System.IO.Directory.GetFiles(pth)
+            Dim bmp As New Bitmap(str)
+            If CheckBox4.Checked Then
+                WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 30)
+                WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 30)
+                WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 30)
+                WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 30)
+            End If
+            If CheckBox3.Checked Then
+                WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 24)
+                WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 24)
+                WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 24)
+                WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 24)
+            End If
+            If CheckBox2.Checked Then
+                WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 16)
+                WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 16)
+                WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 16)
+                WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 16)
+            End If
+            If CheckBox1.Checked Then
+                'WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 8)
+                WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 8)
+                WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 8)
+                WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 8)
+            End If
+        Next
+    End Sub
+
+    Private Sub WriteFile(ByVal bmp As Bitmap, format As Imaging.PixelFormat, ByRef count As Integer, size As Integer)
+        bmp = bmp.Clone(New Rectangle(0, 0, bmp.Width, bmp.Height), format)
+        bmp = New Bitmap(bmp, New Size(size, size))
+        For f = 0 To size - 1
+            bmp.MakeTransparent(bmp.GetPixel(0, f))
+            bmp.MakeTransparent(bmp.GetPixel(size - 1, f))
+            bmp.MakeTransparent(bmp.GetPixel(f, size - 1))
+            bmp.MakeTransparent(bmp.GetPixel(f, 0))
+        Next f
+        Dim outpath As String = TextBox1.Text
+        Dim filename As String = outpath + "\orig" + size.ToString() + "_" + count.ToString() + ".png"
+        bmp.Save(filename, Imaging.ImageFormat.Png)
+        count = count + 1
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dlg As New FolderBrowserDialog
+        dlg.SelectedPath = TextBox2.Text
+        dlg.ShowDialog()
+        TextBox2.Text = dlg.SelectedPath
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim dlg As New FolderBrowserDialog
+        dlg.SelectedPath = TextBox1.Text
+        dlg.ShowDialog()
+        TextBox1.Text = dlg.SelectedPath
+    End Sub
+End Class
+
