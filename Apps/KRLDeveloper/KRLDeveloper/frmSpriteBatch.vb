5437a93fe7205a15243133ed0175c695a6dbfc24
SoftMonster <aj268.wright@googlemail.com>
2024-06-05 13:44:11 +0100

    More KeeperRL 1.0 compatible mods written by me.

--- Apps/KRLDeveloper/KRLDeveloper/frmSpriteBatch.vb
+++ Apps/KRLDeveloper/KRLDeveloper/frmSpriteBatch.vb
@@ -6,22 +6,30 @@
         Dim pth As String = TextBox2.Text
         For Each str As String In System.IO.Directory.GetFiles(pth)
             Dim bmp As New Bitmap(str)
-            WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 30)
-            WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 30)
-            WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 30)
-            WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 30)
-            WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 24)
-            WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 24)
-            WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 24)
-            WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 24)
-            WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 16)
-            WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 16)
-            WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 16)
-            WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 16)
-            WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 8)
-            WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 8)
-            WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 8)
-            WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 8)
+            If CheckBox4.Checked Then
+                WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 30)
+                WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 30)
+                WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 30)
+                WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 30)
+            End If
+            If CheckBox3.Checked Then
+                WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 24)
+                WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 24)
+                WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 24)
+                WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 24)
+            End If
+            If CheckBox2.Checked Then
+                WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 16)
+                WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 16)
+                WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 16)
+                WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 16)
+            End If
+            If CheckBox1.Checked Then
+                'WriteFile(bmp, Imaging.PixelFormat.Format1bppIndexed, count, 8)
+                WriteFile(bmp, Imaging.PixelFormat.Format4bppIndexed, count, 8)
+                WriteFile(bmp, Imaging.PixelFormat.Format8bppIndexed, count, 8)
+                WriteFile(bmp, Imaging.PixelFormat.Format16bppRgb555, count, 8)
+            End If
         Next
     End Sub
 
@@ -40,5 +48,18 @@
         count = count + 1
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dlg As New FolderBrowserDialog
+        dlg.SelectedPath = TextBox2.Text
+        dlg.ShowDialog()
+        TextBox2.Text = dlg.SelectedPath
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim dlg As New FolderBrowserDialog
+        dlg.SelectedPath = TextBox1.Text
+        dlg.ShowDialog()
+        TextBox1.Text = dlg.SelectedPath
+    End Sub
 End Class
 
