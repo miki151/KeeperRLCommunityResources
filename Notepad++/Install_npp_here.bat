059d8083dc7d1b1ed578c496aa7f4591dbdc5926
Andrew Wright <aj268.wright@googlemail.com>
2025-06-29 15:15:41 +0100

    KeeperRL npp settings.

--- Notepad++/Install_npp_here.bat
+++ Notepad++/Install_npp_here.bat
@@ -23,6 +23,35 @@ echo.
 echo Cleaning up...
 del "%NPP_FILE%"
 
+@echo off
+setlocal enabledelayedexpansion
+
+:: Get the directory where the batch file is located
+set "SOURCE_DIR=%~dp0"
+set "DEST_DIR=%SOURCE_DIR%npp\"
+
+:: Make sure destination folder exists
+if not exist "%DEST_DIR%" (
+    mkdir "%DEST_DIR%"
+)
+
+:: Loop through all files and folders except the 'npp' directory
+for /f "delims=" %%I in ('dir "%SOURCE_DIR%" /b /a-d') do (
+    if /i not "%%I"=="copy_to_npp.bat" (
+        copy /Y "%SOURCE_DIR%%%I" "%DEST_DIR%"
+    )
+)
+
+for /f "delims=" %%D in ('dir "%SOURCE_DIR%" /b /ad') do (
+    if /i not "%%D"=="npp" (
+        xcopy /E /I /Y /H "%SOURCE_DIR%%%D" "%DEST_DIR%%%D"
+    )
+)
+
+echo All files and folders (except 'npp') copied to the 'npp' folder.
+pause
+
+
 echo.
 echo Done! Git and Notepad++ have been extracted to:
 echo %NPP_DIR%
