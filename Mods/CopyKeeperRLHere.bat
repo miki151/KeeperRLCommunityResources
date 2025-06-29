4d993be21fd2d74831006aa1338b3b141ec907be
Andrew Wright <aj268.wright@googlemail.com>
2025-06-29 02:09:54 +0100

    rejuggle

--- Mods/CopyKeeperRLHere.bat
+++ Mods/CopyKeeperRLHere.bat
@@ -16,18 +16,6 @@ echo Copying KeeperRL to "%TARGET_DIR%" excluding 'mods' folders...
 robocopy "%SOURCE_DIR%" "%TARGET_DIR%" /E
 
 echo.
-echo Renaming any 'Mods' folders to 'mods' (forcing lowercase)...
-
-:: Search and rename folders named exactly "Mods" (case-insensitive match)
-for /d /r "%TARGET_DIR%" %%F in (*) do (
-    if /i "%%~nxF"=="Mods" (
-        echo Found: %%F
-        ren "%%F" "mod_temp"
-        ren "%%~dpFmod_temp" "mods"
-    )
-)
-
-echo.
 echo Done.
 pause
 exit
--- Mods/DO_NOT_USE_Softmonsters_Backup_A36.bat
+++
@@ -1,3 +0,0 @@
-RMDIR "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36" /S /Q
-mkdir "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36"
-xcopy /s "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods" "C:\PRJ\KeeperRLCommunityResources\Mods\Alpha36"
